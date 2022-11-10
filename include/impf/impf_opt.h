7baaf1eed3eaf1fb1b217a9c49b98176f27508a3
Zhuang Linsheng <zhuanglinsheng@outlook.com>
2022-11-10 11:28:06 +0800

    init-module--impf-opt

---
+++ include/impf/impf_opt.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2022 Zhuang Linsheng <zhuanglinsheng@outlook.com>
+ * License: LGPL 3.0 <https://www.gnu.org/licenses/lgpl-3.0.html>
+ */
+
+/*!****************************************************************************
+ * \file impf_opt.h
+ * \brief Optimization.
+ *****************************************************************************/
+
+#ifndef __IMPF_OPT_H__
+#define __IMPF_OPT_H__
+
+#ifdef __cpluscplus
+extern "C" {
+#endif /* __cplusplus */
+
+/**************************************************************************//**
+ * \addtogroup module_opt
+ *
+ * \details
+ * \cite nocedal1999numerical
+ *
+ * @{
+ *****************************************************************************/
+
+
+/** @} */ /*--- end of module_opt ---*/
+
+#ifdef __cpluscplus
+}
+#endif /* __cpluscplus */
+
+#endif /* __IMPF_OPT_H__ */
+
