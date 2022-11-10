c45ac670866250a2dfb3e8284913a0df6479ec9f
zhuanglinsheng <zhuanglinsheng@outlook.com>
2022-11-10 19:22:26 +0800

    [skip ci] update-doc--interface-root-newton-mfn

--- docs/group__module__root.js
+++ docs/group__module__root.js
@@ -1,6 +1,6 @@
 var group__module__root =
 [
-    [ "impf_subrt_1f1_root_newton", "group__module__root_ga8b377f36c602764197f93e2f8c1628f7.html#ga8b377f36c602764197f93e2f8c1628f7", null ],
-    [ "impf_subrt_2f2_root_newton", "group__module__root_ga7e7c07265c4e0b9c94fce0297feaf5b6.html#ga7e7c07265c4e0b9c94fce0297feaf5b6", null ],
-    [ "impf_subrt_3f3_root_newton", "group__module__root_gac5750996e3df23c724f2b667dcc87d5f.html#gac5750996e3df23c724f2b667dcc87d5f", null ]
+    [ "impf_subrt_root_newton_1f1", "group__module__root_ga90ecf3d317e13a3df2593c94428b3cae.html#ga90ecf3d317e13a3df2593c94428b3cae", null ],
+    [ "impf_subrt_root_newton_2f2", "group__module__root_gaff100f2e9833dd799cd3f9de3c869cb0.html#gaff100f2e9833dd799cd3f9de3c869cb0", null ],
+    [ "impf_subrt_root_newton_3f3", "group__module__root_ga159478f988a5b7e7f677ee02018191f8.html#ga159478f988a5b7e7f677ee02018191f8", null ]
 ];
