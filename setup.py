841b72f65a479943d5deb2299ae860527c03e12c
Zhuang Linsheng <zhuanglinsheng@outlook.com>
2024-07-17 14:01:13 +0800

    create-python-package
    Framework of python wrapper

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='impf',
+	version='1.0',
+	author='Zhuang Linsheng',
+	author_email='zhuanglinsheng@outlook.com',
+	description='',
+	long_description='.',
+	long_description_content_type='text/plain',
+	url='https://github.com/zhuanglinsheng/impf',
+	packages=find_packages(),
+	install_requires=['numpy'],
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: GPL3 License',
+		'Operating System :: OS Independent',
+	],
+)
